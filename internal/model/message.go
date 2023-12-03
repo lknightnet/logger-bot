3e614c59b177bb5c8a9f73437e74f02e5f57416b
lk <lknigtnet@gmail.com>
2023-12-03 19:52:54 +0500

    Fix string

--- internal/model/message.go
+++ internal/model/message.go
@@ -13,11 +13,11 @@ type Message struct {
 
 func (msg *Message) String() string {
 	res := fmt.Sprintf("Message:{\n")
-	res = res + fmt.Sprintf("\tID: %d", msg.ID)
-	res = res + fmt.Sprintf("\tLevel: %s", msg.Level)
-	res = res + fmt.Sprintf("\tText: %s", msg.Text)
-	res = res + fmt.Sprintf("\tMicroService Name: %s", msg.MicroServiceName)
-	res = res + fmt.Sprintf("\tTime: %s", msg.Time)
+	res = res + fmt.Sprintf("\tID: %d\n", msg.ID)
+	res = res + fmt.Sprintf("\tLevel: %s\n", msg.Level)
+	res = res + fmt.Sprintf("\tText: %s\n", msg.Text)
+	res = res + fmt.Sprintf("\tMicroService Name: %s\n", msg.MicroServiceName)
+	res = res + fmt.Sprintf("\tTime: %s\n", msg.Time)
 	res = res + fmt.Sprintf("\tOther: %s\n", msg.Other)
 	res = res + fmt.Sprintf("}")
 	return res
